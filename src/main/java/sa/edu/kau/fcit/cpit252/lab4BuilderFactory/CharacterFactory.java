8dac11367498ee4eb4da32d64d9b5efd591cd0c3
Abdulraheem Bashaweeh <abashaweeh@stu.kau.edu.sa>
2023-09-28 23:09:03 +0300

    committed

--- src/main/java/sa/edu/kau/fcit/cpit252/lab4BuilderFactory/CharacterFactory.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/lab4BuilderFactory/CharacterFactory.java
@@ -4,7 +4,7 @@ package sa.edu.kau.fcit.cpit252.lab4BuilderFactory;
 
 public class CharacterFactory {
 
-    // TODO: Add a factory method that returns an object (Archer, FlagBearer, or Knight) by its type and name:
+    // TODO:: Add a factory method that returns an object (Archer, FlagBearer, or Knight) by its type and name:
     public static Characters createCharacter(CharacterTypes type, String name){
         if(name == null || name.trim().isEmpty()){
             throw new IllegalArgumentException("A character must have a name!");
